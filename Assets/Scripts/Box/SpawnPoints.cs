610a12ea74c2fc5d0819524c40e34ce9da423913
Time1Rift <romas-98@mail.ru>
2024-02-21 18:28:05 +0300

    Test 1.1
    исправил ошибку при старте игры

--- Assets/Scripts/Box/SpawnPoints.cs
+++ Assets/Scripts/Box/SpawnPoints.cs
@@ -10,19 +10,7 @@ public class SpawnPoints : MonoBehaviour
     private void Awake()
     {
         _transform = transform;
-    }
-
-    private void Start()
-    {
-        _points = new Transform[_transform.childCount];
-        Vector3 offset = new Vector3(_offset, 0, 0);
-
-        for (int i = 0; i < _points.Length; i++)
-        {
-            _points[i] = _transform.GetChild(i);
-            _points[i].position += offset;
-            offset.x += _offset;
-        }
+        ArrangePoints();
     }
 
     public Vector3[] GetPoints()
@@ -33,5 +21,18 @@ public class SpawnPoints : MonoBehaviour
             result[i] = _points[i].position;
 
         return result;
+    }
+
+    private void ArrangePoints()
+    {
+        _points = new Transform[_transform.childCount];
+        Vector3 offset = new Vector3(_offset, 0, 0);
+
+        for (int i = 0; i < _points.Length; i++)
+        {
+            _points[i] = _transform.GetChild(i);
+            _points[i].position += offset;
+            offset.x += _offset;
+        }
     }
 }
